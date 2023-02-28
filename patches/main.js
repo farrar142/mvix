9f6c8aaf2e9602fd176314cec6e2244f3b2e2f31
FlandreDaisuki <vbnm123c+github@gmail.com>
2023-03-01 00:58:55 +0800

    feat: save/load mechanism

---
+++ patches/main.js
@@ -0,0 +1,74 @@
+//=============================================================================
+// main.js
+//=============================================================================
+
+const debounce = (fn, wait = 100) => {
+  let timerId;
+
+  return () => {
+    clearTimeout(timerId);
+    timerId = setTimeout(fn, wait);
+  };
+}
+
+const debouncedSave = debounce(() => fetch('/mvix/save', {
+  method: 'POST',
+  body: JSON.stringify(localStorage)
+}))
+
+const proxiedSetItem = (key, value) => {
+  debouncedSave();
+  return Reflect.set(localStorageProxy, key, value)
+}
+
+const proxiedGetItem = (key) => {
+  return Reflect.get(localStorageProxy, key)
+}
+
+const localStorageProxy = new Proxy(window.localStorage,{
+  get: (ls, prop) => {
+    if (prop === 'setItem') {
+      return proxiedSetItem;
+    } else if (prop === 'getItem') {
+      return proxiedGetItem;
+    } else if (prop === 'key') {
+      return (key) => Reflect.apply(ls.key, ls, [key]);
+    } else if (prop === 'clear') {
+      return () => Reflect.apply(ls.clear, ls, []);
+    } else if (prop === 'removeItem') {
+      return (key) => Reflect.apply(ls.removeItem, ls, [key])
+    }
+    return Reflect.get(ls, prop)
+  },
+});
+
+Object.defineProperty(window, 'localStorage', {
+  configurable: true,
+  enumerable: true,
+  value: localStorageProxy,
+});
+
+const loadRemoteGameSave = (remoteGameSave) => {
+  localStorage.clear()
+  for (const k of Object.keys(remoteGameSave)) {
+    localStorage.setItem(k, remoteGameSave[k])
+  }
+}
+
+PluginManager.setup($plugins);
+
+window.onload = async () => {
+  const remoteGameSave = await fetch('/mvix/load', {method: 'POST'}).then((res) => res.json())
+  const currentGame = localStorage.getItem('mvix:current');
+  if(currentGame && currentGame !== document.title) {
+    const shouldOverwrite = confirm(`The save data of "${remoteGameSave['mvix:current']}" will OVERWRITE the save data of "${currentGame}", continue?`);
+    if(!shouldOverwrite) {
+      return document.write('<h1>Please ensure that your previous game save file existed, then overwrite by new game.</h1>')
+    }
+  }
+  if(remoteGameSave) {
+    loadRemoteGameSave(remoteGameSave)
+  }
+  localStorage.setItem('mvix:current', document.title);
+  SceneManager.run(Scene_Boot);
+};
--- server.mjs
+++ server.mjs
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
+import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import open from 'open';
 import Fastify from 'fastify';
-import FastifyStatic from '@fastify/static';
 import { findFileCaseInsensitively, isExistingFile } from './utils.mjs';
 
 /* eslint-disable no-console */
@@ -16,19 +17,43 @@ if (!(await isExistingFile('index.html'))) {
 const PWD = process.cwd();
 const DEBUG_MODE = Boolean(process.env.DEBUG);
 const PORT = Number(process.env.PORT ?? 3000);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const SAVE_DATA_PATH = path.join(PWD, 'mvix.json');
 
 const server = Fastify({ logger: DEBUG_MODE });
 
-server.register(FastifyStatic, {
+await server.register(import('@fastify/static'), {
   root: PWD,
   index: false,
   prefix: '/static/', // FIXME: workaround to FastifyError: Method 'HEAD' already declared for route '/*'
+});
+
+server.post('/mvix/save', async(req, reply) => {
+  if (req.body){
+    await fs.writeFile(SAVE_DATA_PATH, req.body, 'utf8');
+    return reply.status(200);
+  }
+  reply.status(204);
+});
+
+server.post('/mvix/load', async(req, reply) => {
+  if (!await isExistingFile(SAVE_DATA_PATH)) {
+    return reply.send('null');
+  }
+
+  const data = await fs.readFile(SAVE_DATA_PATH, 'utf8');
+  return reply.send(data);
 });
 
 server.get('/*', async(req, reply) => {
   const found = await findFileCaseInsensitively(path.join(PWD, req.url));
   if (found) {
     const { dir, base } = path.parse(found);
+
+    if (base === 'main.js') {
+      return reply.sendFile(base, path.join(__dirname, 'patches'));
+    }
+
     return reply.sendFile(base, dir);
   }
 
